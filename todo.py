0b091684809e65f89e640a3ccc9a58710c27dcee
Housseyn-chalouche <58724606+Housseyn-chalouche@users.noreply.github.com>
2021-05-27 21:01:48 +0100

    Update todo.py

--- todo.py
+++ todo.py
@@ -4,9 +4,9 @@ import sqlite3
 conn = sqlite3.connect('todo.db')
 print('database opend succesfuly')
 
-cur = conn.execute('select * from users join tasks on users.user_id=tasks.user_id where users.user_name='hocine chaloum ')
+cur = conn.execute('select * from users join tasks on users.user_id=tasks.user_id where users.user_name like('hocine%')
 for curser in cur:
     print(cur,'\n')
 
-                   # "select sqlite_version()"
+                   # "select sqlite_version()" sqlite version
 conn.close()
