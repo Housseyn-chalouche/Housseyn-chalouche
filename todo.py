974605a938ce4b64f8efbfcaf277bd414b24158f
housseyn.chalouhe <chalho307@gmail.com>
2021-05-27 20:30:59 +0100

    adding todo tp

---
+++ todo.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+conn = sqlite3.connect('todo.db')
+print('database opend succesfuly')
+
+cur = conn.execute('select * from users join tasks on users.user_id=tasks.user_id where users.user_name='hocine chaloum ' );
+for curser in cur:
+    print(cur,'\n')
+
+conn.close()
